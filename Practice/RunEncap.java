d5d99ca6794a988352df189a6fc122f44f1c8e5a
damngamerz <sauravsinghshakya@yahoo.com>
2017-01-11 23:23:47 +0530

    11 Jan

---
+++ Practice/RunEncap.java
@@ -0,0 +1,10 @@
+public class RunEncap {
+        public static void main(String args[]) {
+                EncapTest encap=new EncapTest();
+                encap.setName("Saurav");
+                encap.setAge(21);
+                encap.setIdNum("12343ms");
+                
+                System.out.println("Name : "+encap.getName() + " Age : "+encap.getAge());
+        }
+}
