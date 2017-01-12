68386e2156b50b29e4b954b3e59396b6bacf1fb0
damngamerz <sauravsinghshakya@yahoo.com>
2017-01-12 20:46:01 +0530

    12 Jan

---
+++ Practice/GenericMethodTest.java
@@ -0,0 +1,23 @@
+public class GenericMethodTest {
+        public static < E > void printArray( E[] inputArray) {
+        for (E element :inputArray) {
+                System.out.printf("%s ", element);
+                }
+        System.out.println();
+        }
+        
+        public static void main(String args[]){
+        Integer[] intArray = {1,2,3,4,5};
+        Double[] doubleArray={1.1,2.2,3.3,4.4,5.5};
+        Character[] charArray = {'H','E','L','L','O'};
+        
+        System.out.println("Array integerArray contains:");
+        printArray(intArray);
+        
+        System.out.println("\nArray dounleArray contains: ");
+        printArray(doubleArray);
+        
+        System.out.println("\nArray caracterArray contains:");
+        printArray(charArray);
+        }
+}
